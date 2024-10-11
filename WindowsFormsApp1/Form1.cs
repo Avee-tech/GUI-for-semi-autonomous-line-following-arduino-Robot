a989f4277b19f1020975955a586398136ee7b038
Avee-tech <66404227+Avee-tech@users.noreply.github.com>
2024-10-11 11:03:53 +0800

    Update Form1.cs
    Updated code

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.KeyPressEventHandler;
+using System.Windows.Controls;
 
 namespace WindowsFormsApp1
 { 
@@ -23,10 +24,11 @@ namespace WindowsFormsApp1
         int state = 0;
         byte status1 = 0;
         byte status2 = 0;
+        bool connected = false;
 
         byte[] Outputs = new byte[4];
         byte[] Inputs = new byte[5];
-        string[] KeysPressed = new string[2];
+        string[] KeysPressed = new string[2];// Max number of keys pressed is two as more than that functions would collide.
 
 
         const byte START = 255;
@@ -45,6 +47,7 @@ namespace WindowsFormsApp1
                     try
                     {
                         serial.Open();                               //Try to connect to the serial.
+                        connected = true;
                     }
                     catch
                     {
@@ -72,6 +75,7 @@ namespace WindowsFormsApp1
 
         private void Key_Output(string[] Keys)
         {
+            //default settings set before the changes are made according to the key presses
             ForwardButton.BackColor = Color.LimeGreen;
             BackButton.BackColor = Color.LimeGreen;
             LeftButton.BackColor = Color.LimeGreen;
@@ -82,7 +86,10 @@ namespace WindowsFormsApp1
             RightLabel.ForeColor = Color.White;
             LeftLabel.ForeColor = Color.White;
 
+            //get the key/keys that where pressed down and set the outputs accordingly and the GUI updated with the key presses
             if ((Keys[0] == "w" && Keys[1] == "a") || (Keys[1] == "w" && Keys[0] == "a")) {
+
+                //if  forward and left button is pressed
                 sendIO(2, (byte)55);//L
                 sendIO(3, (byte)70);//R
                 ForwardButton.BackColor = Color.ForestGreen;
@@ -93,6 +100,8 @@ namespace WindowsFormsApp1
             }
             else if ((Keys[0] == "w" && Keys[1] == "d") || (Keys[1] == "w" && Keys[0] == "d"))
             {
+
+                //if forwards and right button is pressed
                 sendIO(2, (byte)70);//L
                 sendIO(3, (byte)55);//R
                 ForwardButton.BackColor = Color.ForestGreen;
@@ -103,6 +112,8 @@ namespace WindowsFormsApp1
             }
             else if ((Keys[0] == "s" && Keys[1] == "d") || (Keys[1] == "s" && Keys[0] == "d"))
             {
+
+                //if backwards and right button is pressed
                 sendIO(2, (byte)30);//L
                 sendIO(3, (byte)45);//R
                 BackButton.BackColor = Color.ForestGreen;
@@ -113,6 +124,8 @@ namespace WindowsFormsApp1
             }
             else if ((Keys[0] == "s" && Keys[1] == "a") || (Keys[1] == "s" && Keys[0] == "a"))
             {
+
+                //if backwards and left button is pressed
                 sendIO(2, (byte)45);//L
                 sendIO(3, (byte)30);//R
                 BackButton.BackColor = Color.ForestGreen;
@@ -123,6 +136,8 @@ namespace WindowsFormsApp1
             }
             else if (Keys[0] == "w" || Keys[1] == "w")
             {
+
+                //if forwards button is pressed
                 sendIO(2, (byte)70);//L
                 sendIO(3, (byte)70);//R
                 ForwardButton.BackColor = Color.ForestGreen;
@@ -131,6 +146,8 @@ namespace WindowsFormsApp1
             }
             else if (Keys[0] == "s" || Keys[1] == "s")
             {
+                
+                //if backwards button is pressed
                 sendIO(2, (byte)30);//L
                 sendIO(3, (byte)30);//R
                 BackButton.BackColor = Color.ForestGreen;
@@ -139,6 +156,8 @@ namespace WindowsFormsApp1
             }
             else if (Keys[0] == "a" || Keys[1] == "a")
             {
+
+                // if left butto is pressed
                 sendIO(2, (byte)30);//L
                 sendIO(3, (byte)70);//R
                 LeftButton.BackColor = Color.ForestGreen;
@@ -147,6 +166,8 @@ namespace WindowsFormsApp1
             }
             else if (Keys[0] == "d" || Keys[1] == "d")
             {
+
+                //if right button is pressed
                 sendIO(2, (byte)70);//L
                 sendIO(3, (byte)30);//R
                 RightButton.BackColor = Color.ForestGreen;
@@ -155,17 +176,23 @@ namespace WindowsFormsApp1
             }
             else if (Keys[0] == "" && Keys[1] == "")
             {
+
+                //if no buttons are pressed robot will stop
                 sendIO(2, (byte)50);//L
                 sendIO(3, (byte)50);//R
                 statusBox1.Text = "Stop";
             }
         }
 
-
+        //function called when any button is pressed, arguments include the button which was pressed down
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+
+            //This is where the button pressed down is registered and saved into the Keypressed list
             if (e.KeyCode == Keys.W)
             {
+                //if command makes sure the same button click is not registered twice and that the forwards and backwards
+                //button are not clicked simultaneously registering only the latest one
                 if (KeysPressed[0] == "" || KeysPressed[0] == "s" || KeysPressed[0] == "w")
                 {
                     KeysPressed[0] = "w";
@@ -208,6 +235,9 @@ namespace WindowsFormsApp1
                     KeysPressed[1] = "d";
                 }
             }
+
+            //if the current state is set to manual the registeres buttons
+            //will run the code to output the button clicks to the arduino for the robot to move
             if (state == 0)
             {
                 Key_Output(KeysPressed);
@@ -216,6 +246,9 @@ namespace WindowsFormsApp1
         }
 
 
+// Code that allows the user to click on the buttons to make changes int he robots movement.
+//section was removed as the keyboard controls proved more practical
+/*
         private void ForwardButton_Click(object sender, EventArgs e)
         {
             if (state == 0)
@@ -260,14 +293,15 @@ namespace WindowsFormsApp1
                 sendIO(3, (byte)25);//R
             }
         }
+*/
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
 
+        //Function to be run when the key is released
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+
+            //this function removes the pressed key from the list making it available
+            //to the next pressed key
             if (e.KeyCode == Keys.W)
             {
                 if (KeysPressed[0] == "w")
@@ -324,10 +358,23 @@ namespace WindowsFormsApp1
 
         private void IOtimer_tick_Tick(object sender, EventArgs e)
         {
-            
 
-            if (serial.IsOpen) //Check that a serial connection exists.
+            try
             {
+                serial.Open();                               //Try to connect to the serial.
+                connected = true;
+            }
+            catch
+            {
+                statusBox1.Enabled = false;
+            }
+
+
+            if (serial.IsOpen && connected) //Check that a serial connection exists.
+            {
+                Errorpanel.Visible = false;
+                ErrorLabel.Visible = false;
+
                 if (serial.BytesToRead >= 5) //Check that the buffer contains a full four byte package.
                 {
                     //statusBox.Text = "Incoming"; // A status box can be used for debugging code.
@@ -351,6 +398,8 @@ namespace WindowsFormsApp1
 
                             if (state == 1)
                             {
+
+                                //Reset all the colours to original state
                                 ForwardLabel.ForeColor = Color.White;
                                 BackLabel.ForeColor = Color.White;
                                 RightLabel.ForeColor = Color.White;
@@ -358,67 +407,117 @@ namespace WindowsFormsApp1
 
                                 SensorLeft.BackColor = Color.Black;
                                 SensorRight.BackColor = Color.Black;
+
+
                                 switch (Inputs[3])
                                 {
-                                    case 38:
+                                    case 38:// State == 1
                                         statusBox1.Text = "Forwards";
                                         ForwardLabel.ForeColor = Color.Red;
                                         SensorLeft.BackColor = Color.White;
                                         SensorRight.BackColor = Color.White;
                                         break;
 
-                                    case 40:
+                                    case 40:// State == 2
                                         statusBox1.Text = "Sway Right";
                                         ForwardLabel.ForeColor = Color.Red;
                                         RightLabel.ForeColor = Color.Red;
                                         SensorRight.BackColor = Color.White;
                                         break;
 
-                                    case 42:
+                                    case 42:// State == 3
                                         statusBox1.Text = "Sway Left";
                                         ForwardLabel.ForeColor = Color.Red;
                                         LeftLabel.ForeColor = Color.Red;
                                         SensorLeft.BackColor = Color.White;
                                         break;
 
-                                    case 44:
+                                    case 44:// State == 4
                                         statusBox1.Text = "Turning Right";
                                         RightLabel.ForeColor = Color.Red;
                                         break;
 
-                                    case 46:
+                                    case 46://state == 5
                                         statusBox1.Text = "Turning Left";
                                         LeftLabel.ForeColor = Color.Red;
                                         break;
 
                                 }
                             }
+
                             status1 = Inputs[1];
                             status2 = Inputs[2];
-                            progressBar1.Value = status1;
-                            progressBar2.Value = status2;
+
+                            //as two progress bars are used to show the speeds in the forwards
+                            // and backwards direction
+                            if (status1 > 50)
+                            {
+                                progressBar1.Value = (status1 - 50);
+                                progressBar3.Value = 0;
+                            }
+                            else
+                            {
+                                progressBar3.Value = 50 - status1;
+                                progressBar1.Value = 0;
+                            }
+                            if (status2 > 50)
+                            {
+                                progressBar2.Value = (status2- 50);
+                                progressBar4.Value = 0;
+                            }
+                            else
+                            {
+                                progressBar4.Value = 50 - status2;
+                                progressBar2.Value = 0;
+                            }
                         }
                     }
                 }
             }
+            else
+            {
+                // If the serial port is closed an error msg will appear to warn the user
+                Errorpanel.Visible = true;
+                ErrorLabel.Visible = true;
+            }
         }
 
 
+        // Sets the mode of the robot from AUTO to manual or vise versa
         private void button1_Click_1(object sender, EventArgs e)
         {
             if (state == 1)
             {
                 state = 0;
                 button1.Text = "Manual";
-                button1.BackColor = Color.Cyan;
+                button1.BackColor = Color.LimeGreen;
+                panel1.BackColor = Color.Silver;
+                ForwardButton.BackColor = Color.LimeGreen;
+                BackButton.BackColor = Color.LimeGreen;
+                LeftButton.BackColor = Color.LimeGreen;
+                RightButton.BackColor = Color.LimeGreen;
             }
             else if (state == 0)
             {
                 state = 1;
                 button1.Text = "AUTO";
                 button1.BackColor = Color.Yellow;
+                panel1.BackColor = Color.LightGray;
+                ForwardButton.BackColor = Color.PaleGreen;
+                BackButton.BackColor = Color.PaleGreen;
+                LeftButton.BackColor = Color.PaleGreen;
+                RightButton.BackColor = Color.PaleGreen;
+
             }
             sendIO(4, (byte)state);
+        }
+
+        //Button to reset the threshold point
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sendIO(5, (byte)0);
+            SensorLeft.BackColor = Color.Red;
+            SensorRight.BackColor = Color.Red;
         }
     }
 }
